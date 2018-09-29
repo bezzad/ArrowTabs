277b6c902381fbabba104215d14684c97b759c4f
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2018-09-29 15:06:04 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+$(".arrow-tabs").tabs({ 
+  show: { effect: "slide", direction: "left", duration: 200, easing: "easeOutBack" } ,
+  hide: { effect: "slide", direction: "right", duration: 200, easing: "easeInQuad" } 
+});
